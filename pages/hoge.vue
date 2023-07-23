cb7a43897a17a6ba7dcdcb8cd52fbd5868eb638d
konkarin <konkarin.photo@gmail.com>
2023-07-23 15:14:42 +0900

    feat: 🎸 add pages

---
+++ pages/hoge.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <div>
+      <nuxt-link to="/">to root</nuxt-link>
+    </div>
+    hoge page
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>{{ data }}</div>
+  <div>
+    <div>
+      <nuxt-link to="/hoge">to hoge</nuxt-link>
+    </div>
+    {{ data }}
+  </div>
 </template>
 
 <script lang="ts">
@@ -12,6 +17,8 @@ export default defineComponent({
       console.log('useLazyAsyncData')
       return Promise.resolve('data')
     })
+
+    console.log(data.value)
 
     return { data }
   },

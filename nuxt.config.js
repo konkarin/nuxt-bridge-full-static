e08a7eccc64cee002b58fb28814a4eaf4780e32f
konkarin <konkarin.photo@gmail.com>
2023-07-23 12:57:31 +0900

    feat: 🎸 init full static generation

---
+++ nuxt.config.js
@@ -0,0 +1,26 @@
+import { defineNuxtConfig } from '@nuxt/bridge'
+
+export default defineNuxtConfig({
+  bridge: {
+    capi: true,
+    nitro: false,
+  },
+
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'nuxt-bridge-test',
+    htmlAttrs: {
+      lang: 'en',
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+  },
+})
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>{{ data }}</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, useLazyAsyncData } from '#imports'
+
+export default defineComponent({
+  name: 'IndexPage',
+  setup() {
+    const { data } = useLazyAsyncData('index', () => {
+      console.log('useLazyAsyncData')
+      return Promise.resolve('data')
+    })
+
+    return { data }
+  },
+})
+</script>
